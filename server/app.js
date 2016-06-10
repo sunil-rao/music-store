31eb0d95a79cee6fe68218018990a8fdca79a53d
Sunil Rao <sunil.elite11@gmail.com>
2016-06-10 19:44:56 +1000

    Serving static files. Got a home page for Music ready

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 var express=require('express');
-var router=require('./routes/musicRouter');
+var musicRouter=require('./routes/musicRouter');
 
 var app=express();
 app.set('views', ['server/views']);
 app.set('view engine', 'ejs');
 
-app.use('/', router);
+
+
+app.use('/static/css', express.static('./bower_components/bootstrap/dist/css'));
+app.use('/static/js', express.static('./bower_components/bootstrap/dist/js'));
+app.use('/music', musicRouter);
 
 app.listen(5000, function(){
-
+console.log(__dirname);
 	console.log('Listening on port 5000');
 });
